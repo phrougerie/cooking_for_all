77a47eb75bff87ac04eb50ceb8fb7b4f64813589
phrougerie <philippe.rougerie@etu.uca.fr>
2019-02-01 12:05:44 +0100

    Decouverte d'Android studio. Ajout de quelqies boutons et test de responsivité. Classe Recipe commencée.

---
+++ Code/app/src/main/java/com/example/cookingforall/Modele/Recipe.java
@@ -0,0 +1,12 @@
+package com.example.cookingforall.Modele;
+
+import android.media.Image;
+
+public class Recipe {
+    private String ingredient[];
+    private String steps[];
+    private Image photos[];
+
+
+
+}
